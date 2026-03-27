3a1efabf83c45fa3d9d287ec4ea551ed8cbfcca0
robelzew <zewrob74@gmail.com>
2026-03-27 23:17:16 +0300

    Add frontend Signup/Login components

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { signupUser, loginUser, getMe } from "../controllers/auth.js";
 import authMiddleware from "../middleware/auth.js";
+import { googleLogin } from "../controllers/auth.js";
 
 const router = express.Router();
 
 router.post("/signup", signupUser);
 router.post("/login", loginUser);
+router.post("/google-login", googleLogin);
 router.get("/me", authMiddleware, getMe);
 
 export default router;
